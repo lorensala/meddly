14d58e4521439224b6fc57b30df30e71b2cbad97
Lorenzo <salalorennn@gmail.com>
2023-06-29 11:15:15 -0300

    Sort dropdowns

--- packages/measurement/lib/src/models/measurement_type.dart
+++ packages/measurement/lib/src/models/measurement_type.dart
@@ -1,72 +1,32 @@
 import 'package:measurement/measurement.dart';
 
 enum MeasurementType {
-  bloodPressure([MeasurementUnit.mmHg]),
   bloodGlucose([MeasurementUnit.mgPerDL]),
-  weight([
-    MeasurementUnit.kg,
-    MeasurementUnit.pounds,
-  ]),
-  height([
-    MeasurementUnit.cm,
-    MeasurementUnit.inches,
-  ]),
-  temperature([
-    MeasurementUnit.celsius,
-    MeasurementUnit.fahrenheit,
-  ]),
-  heartRate([MeasurementUnit.bpm]),
-  oxygenSaturation([MeasurementUnit.percentage]),
-  respiratoryRate([MeasurementUnit.breathsPerMin]),
-  cholesterol([MeasurementUnit.mgPerDL]),
+  bloodPressure([MeasurementUnit.mmHg]),
   bodyMassIndex([MeasurementUnit.kgPerSquareMeter]),
-  visionAcuity([
-    MeasurementUnit.snellen,
-    MeasurementUnit.logMAR,
-  ]),
+  cholesterol([MeasurementUnit.mgPerDL]),
+  electrolyteLevels([MeasurementUnit.percentage]),
+  glucoseTolerance([MeasurementUnit.mgPerDL]),
   hearingAcuity([MeasurementUnit.decibels]),
-  urineAnalysis([
-    MeasurementUnit.percentage,
-  ]),
-  liverFunction([
-    MeasurementUnit.percentage,
-  ]),
-  kidneyFunction([
-    MeasurementUnit.percentage,
-  ]),
-  thyroidFunction([
-    MeasurementUnit.percentage,
-  ]),
-  electrolyteLevels([
-    MeasurementUnit.percentage,
-  ]),
-  whiteBloodCellCount([
-    MeasurementUnit.cellsPerUL,
-  ]),
-  redBloodCellCount([
-    MeasurementUnit.millionsPerUL,
-  ]),
-  plateletCount([
-    MeasurementUnit.thousandPerUL,
-  ]),
-  hemoglobinLevel([
-    MeasurementUnit.gramsPerDL,
-  ]),
-  glucoseTolerance([
-    MeasurementUnit.mgPerDL,
-  ]),
-  vitaminDLevels([
-    MeasurementUnit.ngPerML,
-  ]),
-  ironLevels([
-    MeasurementUnit.microgramsPerDL,
-  ]),
-  prostateSpecificAntigen([
-    MeasurementUnit.ngPerML,
-  ]),
-  other([
-    MeasurementUnit.other,
-  ]);
+  heartRate([MeasurementUnit.bpm]),
+  height([MeasurementUnit.cm, MeasurementUnit.inches]),
+  hemoglobinLevel([MeasurementUnit.gramsPerDL]),
+  ironLevels([MeasurementUnit.microgramsPerDL]),
+  kidneyFunction([MeasurementUnit.percentage]),
+  liverFunction([MeasurementUnit.percentage]),
+  other([MeasurementUnit.other]),
+  oxygenSaturation([MeasurementUnit.percentage]),
+  plateletCount([MeasurementUnit.thousandPerUL]),
+  prostateSpecificAntigen([MeasurementUnit.ngPerML]),
+  redBloodCellCount([MeasurementUnit.millionsPerUL]),
+  respiratoryRate([MeasurementUnit.breathsPerMin]),
+  temperature([MeasurementUnit.celsius, MeasurementUnit.fahrenheit]),
+  thyroidFunction([MeasurementUnit.percentage]),
+  urineAnalysis([MeasurementUnit.percentage]),
+  visionAcuity([MeasurementUnit.snellen, MeasurementUnit.logMAR]),
+  vitaminDLevels([MeasurementUnit.ngPerML]),
+  weight([MeasurementUnit.kg, MeasurementUnit.pounds]),
+  whiteBloodCellCount([MeasurementUnit.cellsPerUL]);
 
   final List<MeasurementUnit> units;
 
